199c390f9ac2a9c2227007f2f01dff87e832733c
TunaLobster <charliepjohnson@gmail.com>
2017-02-11 14:08:53 -0600

    XFoil subrocess automation. Started from https://hakantiftikci.wordpress.com/2010/12/21/using-xfoil-and-automating-via-python-subprocess-module/

---
+++ xfoilautomate/xfoil_subprocess.py
@@ -0,0 +1,41 @@
+import subprocess as sp
+
+foils = ['0009', '0012', '0015', '2415', '4412', '4415', '23012']
+
+ps = sp.Popen([r'C:\Users\charl\Desktop\XFOIL6.99\xfoil.exe'], stdin=sp.PIPE, stdout=None, stderr=None)
+
+
+def issueCmd(cmd, echo=True):
+    ps.stdin.write('{0}\n'.format(cmd).encode())  # Definitely better ways to encode, but nah.
+    if echo:
+        print(cmd)
+
+
+def quitxfoil():
+    issueCmd('QUIT')
+    print('exited XFoil')
+
+
+def x1():
+    for i in foils:
+        issueCmd('NACA ' + i)  # select foil
+        issueCmd('OPER')  # open oper menu
+        issueCmd('ITER 1000')
+        issueCmd('PACC')  # polar accumulation
+        issueCmd(i + 'invisc.txt')  # output file
+        issueCmd('')  # no dump file
+        issueCmd('ASEQ')  # alfa sequence
+        issueCmd('0')  # start alpha
+        issueCmd('12')  # end alpha
+        issueCmd('1')  # alpha step
+        issueCmd('')  # xfoil menu
+        issueCmd('')  # double check to main menu
+
+
+def x2():
+    pass
+    issueCmd('')
+
+
+x1()
+x2()
