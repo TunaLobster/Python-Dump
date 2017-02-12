a8212d44d8b67ac34d67641b6781efeda1c2727d
TunaLobster <charliepjohnson@gmail.com>
2017-02-11 19:51:53 -0600

    Add plotting Add X2

--- xfoilautomate/xfoil_subprocess.py
+++ xfoilautomate/xfoil_subprocess.py
@@ -22,21 +22,57 @@ def x1():
         issueCmd('OPER')  # open oper menu
         issueCmd('ITER 1000')
         issueCmd('PACC')  # polar accumulation
-        issueCmd(i + 'invisc.txt')  # output file
+        issueCmd('naca' + i + 'invisc.txt')  # output file
         issueCmd('')  # no dump file
         issueCmd('ASEQ')  # alfa sequence
         issueCmd('0')  # start alpha
         issueCmd('12')  # end alpha
-        issueCmd('1')  # alpha step
-        issueCmd('PACC')
+        issueCmd('.5')  # alpha step
+        issueCmd('PACC')  # disable polar accumulation
+        issueCmd('PPLO')  # plot polars, lift curve
         issueCmd('')  # xfoil menu
         issueCmd('')  # double check to main menu
+    issueCmd('PLOP')  # enter plot options menu
+    issueCmd('C, F')  # add color option to postscript file
+    issueCmd('')  # xfoil menu
+    issueCmd('OPER')  # open oper menu
+    issueCmd('HARD')  # generate plot.ps file
+    issueCmd('')  # xfoil menu
+    issueCmd('')  # double check to main menu
 
 
-def x2():
-    pass
+def x2(re):  # re as string
+    issueCmd('NACA 4512')  # dummy foil to turn on viscosit
+    issueCmd('OPER')
+    issueCmd('VISC')
+    issueCmd(re)
     issueCmd('')
+    for i in foils:
+        issueCmd('NACA ' + i)  # select foil
+        issueCmd('OPER')  # open oper menu
+        # issueCmd('RE')  # set Reynolds Number, Re
+        # issueCmd(re)
+        issueCmd('ITER 1000')  # change iteration limit
+        issueCmd('PACC')  # polar accumulation
+        issueCmd('naca' + i + 'visc.txt')  # output file
+        issueCmd('')  # no dump file
+        issueCmd('ASEQ')  # alfa sequence
+        issueCmd('0')  # start alpha
+        issueCmd('12')  # end alpha
+        issueCmd('.5')  # alpha step
+        issueCmd('PACC')  # disable polar accumulation
+        issueCmd('PPLO')  # plot polars, lift curve
+        issueCmd('')  # xfoil menu
+        issueCmd('')  # double check to main menu
+    issueCmd('PLOP')  # enter plot options menu
+    issueCmd('C, F')  # add color option to postscript file
+    issueCmd('')  # xfoil menu
+    issueCmd('OPER')  # open oper menu
+    issueCmd('HARD')  # generate plot.ps file
+    issueCmd('')  # xfoil menu
+    issueCmd('')  # double check to main menu
 
-
-x1()
-x2()
+# must be run separately or each will be appended to plot.ps for a very weird looking graph.
+# need to make feature request to xfoil to name HARD command .ps output
+# x1()
+x2(500000)
