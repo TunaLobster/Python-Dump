afa64ee65e7361f8bcf7563ebd221b4ebfcb880a
TunaLobster <charliepjohnson@gmail.com>
2018-10-10 16:18:10 -0500

    Small updates for more use cases

--- BrightspaceHelpers.py
+++ BrightspaceHelpers.py
@@ -3,9 +3,6 @@ import os
 import xlsxwriter
 from fuzzywuzzy import fuzz
 
-# external file with a list of solutions copied and pasted from the internet
-import solutions
-
 
 def filenameparser(filename):
     number, name, date, submission = filename.split(' - ', 3)
@@ -14,12 +11,12 @@ def filenameparser(filename):
 
 
 def splitfilenames(path, ext):
-    '''
+    """
 
     :param path: Directory sting where the submitted assignments were downloaded and extracted to
     :param ext: Tuple of stings of file extensions to scan for. Example: ('.py') or ('.sldprt','.sldasm')
     :return:
-    '''
+    """
     allfiles = os.listdir(path)
     data = [['number', 'lastname', 'firstname', 'data', 'filename submitted']]
     for filename in allfiles:
@@ -42,6 +39,8 @@ def removenumbers(path, ext):
 
 def write2excel(path, data, outputname):
     # note that xlsxwriter cannot overwrite existing files
+    if not outputname.lower().endswith('xlsx'):
+        outputname = outputname.join('.xlsx')
     workbook = xlsxwriter.Workbook(os.path.join(path, outputname))
     worksheet = workbook.add_worksheet()
     for row in range(len(data)):
@@ -50,7 +49,12 @@ def write2excel(path, data, outputname):
     workbook.close()
 
 
-def scanforsoltuions(path, ext, solutionslist, percent):
+def scanforsoltuions(path, ext, percent):
+    # external file with a list of solutions copied and pasted from the internet in a python list.
+    # Example "solutions.py" file contents:
+    # solutionlist = ['''copied from google result''','''students shouldn't do this''','''so many ways to tell''']
+    import solutions
+    solutionslist = solutions.solutionlist
     allfiles = os.listdir(path)
     for filename in allfiles:
         if not filename.lower().endswith(ext):
@@ -65,17 +69,17 @@ def scanforsoltuions(path, ext, solutionslist, percent):
 
 
 def __example__():
-    path = r'C:\Users\Charlie\Dropbox\Graduate\GTA\3403F18\Homework 4 - Upload Download Oct 2, 2018 402 PM'
-    extension = '.py'
-    outputfilename = '3403F18'
-    solutionslist = solutions.solutionlist
+    path = r''
+    extension = ''
+    outputfilename = ''
     percentsimilar = 85  # 85 seemed to turn up the most likely candidates. It's not perfect though
+
     studentfilenamedata = splitfilenames(path, extension)
     write2excel(path, studentfilenamedata, outputfilename)
-    solutionsfound = scanforsoltuions(path, extension, solutionslist, percentsimilar)
-    for i in solutionsfound:
-        print(i)
-    # for i in filenamedata:
+
+    # only really useful when the submissions are code or text files
+    # solutionsfound = scanforsoltuions(path, extension, percentsimilar)
+    # for i in solutionsfound:
     #     print(i)
 
     # DO THIS LAST (For now at least. I'll fix it later.)
